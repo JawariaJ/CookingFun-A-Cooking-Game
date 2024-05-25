0aa54c044e7d768101cc2e8ef63b2ab81f7d8d98
JawariaJ <jawaria.py@gmail.com>
2024-05-26 01:59:15 +0500

    CookingFun: Game

---
+++ src/CookingFun/BurgerMania.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CookingFun;
+
+/**
+ *
+ * @author DR.SHER
+ */
+public class BurgerMania {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Start_Page t = new Start_Page ();
+       t.setVisible(true);
+    }
+    
+}
